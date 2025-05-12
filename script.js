3a8ea148b63b9230cee70d57965ad841e157483a
Jolut11 <jolut11@gmail.com>
2025-05-12 17:12:15 -0500

    Integrando la api 5

--- script.js
+++ script.js
@@ -1,39 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const preguntaTxt = document.getElementById("questionTxt");
+    const preguntaTxt = document.getElementById("questionTxt");
 
-  fetch("https://fr-app.onrender.com/get_cuestionary")
-    .then(res => res.json())
+    fetch("https://fr-app.onrender.com/get_cuestionary")
+    .then(response => {
+        if (!response.ok) {
+            preguntaTxt.textContent = "Respuesta de red no fue ok";
+            throw new Error("Respuesta de red no fue ok");
+        }
+        return response.json();
+        })
     .then(data => {
-        preguntaTxt.textContent = "EEEEEEEEERRROR";
-        preguntaTxt.textContent = data
+        if (data.questions && data.questions.length > 0) {
+            preguntaTxt.textContent = data.questions[0].s; // Cambia el texto
+        } else {
+            preguntaTxt.textContent = "No se encontraron preguntas.";
+        }
     })
     .catch(error => {
-        preguntaTxt.textContent = "EEEEEEEEERRROR";
-        preguntaTxt.textContent = error;
-        console.error("Error: ", error);
+        preguntaTxt.textContent = "Error al obtener preguntas: " + error.message;
+        console.error("Error al obtener preguntas:", error);
     });
 });
-
-// Función para renderizar las preguntas y opciones
-/*function renderQuestions(questions) {
-  const container = document.getElementById("preguntas-container");
-  questions.forEach(question => {
-    const questionElement = document.createElement("div");
-    questionElement.classList.add("pregunta");
-
-    // Agregar la pregunta
-    const pregunta = document.createElement("h3");
-    pregunta.textContent = question.s;
-    questionElement.appendChild(pregunta);
-
-    // Agregar las opciones
-    question.o.forEach(option => {
-      const optionElement = document.createElement("p");
-      optionElement.textContent = `${option.id}: ${option.t}`;
-      questionElement.appendChild(optionElement);
-    });
-
-    // Agregar la pregunta renderizada al contenedor
-    container.appendChild(questionElement);
-  });
-}*/
