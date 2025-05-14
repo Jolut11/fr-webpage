403524423129b7e89b204cb37de542b2d771377e
Jolut11 <jolut11@gmail.com>
2025-05-14 17:52:10 -0500

    Update visual

--- script.js
+++ script.js
@@ -1,34 +1,65 @@
 document.addEventListener("DOMContentLoaded", () => {
     const preguntaTxt = document.getElementById("questionTxt");
     const tituloTxt = document.getElementById("titleTxt");
-    const containerOpciones = document.getElementById("optionsContainer");
-    const ayudaTxt = document.getElementById("ayudaTxt");
+    const formOpciones = document.getElementById("formOptions");
+    const ayudaTxt = document.getElementById("helpTxt");
 
     fetch("https://fr-app.onrender.com/get_cuestionary")
-    .then(response => {
-        if (!response.ok) {
-            preguntaTxt.textContent = "Respuesta de red no fue ok";
-            throw new Error("Respuesta de red no fue ok");
-        }
-        return response.json();
+        .then(response => {
+            if (!response.ok) {
+                preguntaTxt.textContent = "Respuesta de red no fue ok";
+                throw new Error("Respuesta de red no fue ok");
+            }
+            return response.json();
         })
-    .then(data => {
-        tituloTxt.textContent = "Pregunta " + 1 + " de " + data[0].o.length;
-        preguntaTxt.textContent = data[0].s;
-        ayudaTxt.textContent = data[0].j;
+        .then(data => {
+            tituloTxt.textContent = "Pregunta " + 1 + " de " + data[0].o.length;
+            preguntaTxt.textContent = data[0].s;
+            ayudaTxt.textContent = data[0].j;
 
-        for (const opcion of data[0].o) {
-            const optionDiv = document.createElement("div");
-            optionDiv.className = "question-text";
-            const optionText = document.createElement("p");
-            optionText.className = "question-text";
-            optionText.textContent = JSON.stringify(opcion.id) + " - " + JSON.stringify(opcion.t);
-            optionDiv.appendChild(optionText);
-            containerOpciones.appendChild(optionDiv);
-        }
-    })
-    .catch(error => {
-        preguntaTxt.textContent = "Error al obtener preguntas: " + error.message;
-        console.error("Error al obtener preguntas:", error);
-    });
+            const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            data[0].o.forEach((opcion, index) => {
+                const valor = letras[index]; // A, B, C, ...
+
+                const div = document.createElement("div");
+                div.className = "option";
+
+                const label = document.createElement("label");
+
+                const input = document.createElement("input");
+                input.type = "radio";
+                input.name = "respuesta";
+                input.value = valor;
+
+                label.appendChild(input);
+                label.append(` ${JSON.stringify(opcion.t)}`);
+
+                div.appendChild(label);
+                formOpciones.appendChild(div);
+            });
+
+
+            /*for (const opcion of data[0].o) {
+                const optionDiv = document.createElement("div");
+                optionDiv.className = "question-text";
+                const optionText = document.createElement("p");
+                optionText.className = "question-text";
+                optionText.textContent = JSON.stringify(opcion.id) + " - " + JSON.stringify(opcion.t);
+                optionDiv.appendChild(optionText);
+                containerOpciones.appendChild(optionDiv);
+            }*/
+        })
+        .catch(error => {
+            preguntaTxt.textContent = "Error al obtener preguntas: " + error.message;
+            console.error("Error al obtener preguntas:", error);
+        });
 });
+
+function onHelpBtnClic() {
+    document.getElementById('modal').style.display = 'flex';
+}
+
+function cerrarModal() {
+    document.getElementById('modal').style.display = 'none';
+}
