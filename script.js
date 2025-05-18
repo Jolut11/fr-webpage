fc3d4d51cae876cf398402b7fc161186c50b45ad
Jolut11 <jolut11@gmail.com>
2025-05-18 10:46:42 -0500

    Se añade paginación

--- script.js
+++ script.js
@@ -1,74 +1,115 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const spinner = document.getElementById("spinner");
-    const content = document.getElementById("content");
+let questionsData = [];
+let currentQuestionIndex = 0;
 
-    const preguntaTxt = document.getElementById("questionTxt");
-    const tituloTxt = document.getElementById("titleTxt");
-    const formOpciones = document.getElementById("formOptions");
-    const ayudaTxt = document.getElementById("helpTxt");
+spinner = HTMLElement;
+content = HTMLElement;
+
+preguntaTxt = HTMLElement;
+tituloTxt = HTMLElement;
+formOpciones = HTMLElement;
+ayudaTxt = HTMLElement;
+
+document.addEventListener("DOMContentLoaded", () => {
+    spinner = document.getElementById("spinner");
+    content = document.getElementById("content");
+
+    preguntaTxt = document.getElementById("questionTxt");
+    tituloTxt = document.getElementById("titleTxt");
+    formOpciones = document.getElementById("formOptions");
+    ayudaTxt = document.getElementById("helpTxt");
 
     spinner.style.display = "block";
     content.style.display = "none";
 
-    const payload = {
-        number_questions: 1
-    };
-
-    fetch("https://fr-app.onrender.com/create_cuestionary", {
-        method: "POST", headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(payload)
-    })
-        .then(response => {
-            content.style.display = "block";
-            spinner.style.display = "none";
-
-            if (!response.ok) {
-                preguntaTxt.textContent = "Respuesta de red no fue ok";
-                throw new Error("Respuesta de red no fue ok");
-            }
-            return response.json();
-        })
-        .then(data => {
-            tituloTxt.textContent = "Pregunta " + 1 + " de " + data.length;
-            preguntaTxt.textContent = data[0].s;
-            ayudaTxt.textContent = data[0].j;
-
-            const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-            data[0].o.forEach((opcion, index) => {
-                if (JSON.stringify(opcion.t) == "\"\"") {
-                    return;
-                }
-
-                const valor = letras[index]; // A, B, C, ...
-
-                const div = document.createElement("div");
-                div.className = "option";
-
-                const label = document.createElement("label");
-
-                const input = document.createElement("input");
-                input.type = "radio";
-                input.name = "respuesta";
-                input.value = valor;
-
-                label.appendChild(input);
-                label.append(opcion.t);
-
-                div.appendChild(label);
-                formOpciones.appendChild(div);
-            });
-        })
-        .catch(error => {
-            preguntaTxt.textContent = "Error al obtener preguntas: " + error.message;
-            console.error("Error al obtener preguntas:", error);
-        });
+    loadQuestionary(3);
 });
+
+async function loadQuestionary(number_questions) {
+    try {
+        const payload = {
+            number_questions: number_questions
+        };
+
+        const response = await fetch("https://fr-app.onrender.com/create_cuestionary", {
+            method: "POST", headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(payload)
+        });
+
+        if (!response.ok) {
+            throw new Error("Respuesta de red no fue ok");
+        }
+
+        content.style.display = "block";
+        spinner.style.display = "none";
+
+        const data = await response.json();
+        questionsData = data;
+
+        renderQuestion(currentQuestionIndex);
+    } catch (error) {
+        console.error("Error al cargar el cuestionario:", error);
+        alert("Error al cargar el cuestionario. Por favor, inténtalo de nuevo más tarde." + error);
+    }
+}
+
+function renderQuestion(questionIndex) {
+    if (questionIndex > questionsData.length) {
+        return;
+    }
+
+    formOpciones.innerHTML = "";
+
+    tituloTxt.textContent = "Pregunta " + (1 + questionIndex) + " de " + questionsData.length;
+    preguntaTxt.textContent = questionsData[questionIndex].s;
+    ayudaTxt.textContent = questionsData[questionIndex].j;
+
+    const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    questionsData[questionIndex].o.forEach((opcion, index) => {
+        if (JSON.stringify(opcion.t) == "\"\"") {
+            return;
+        }
+
+        const valor = letras[index]; // A, B, C, ...
+
+        const div = document.createElement("div");
+        div.className = "option";
+
+        const label = document.createElement("label");
+
+        const input = document.createElement("input");
+        input.type = "radio";
+        input.name = "respuesta";
+        input.value = valor;
+
+        label.appendChild(input);
+        label.append(opcion.t);
+
+        div.appendChild(label);
+        formOpciones.appendChild(div);
+    });
+}
 
 function onHelpBtnClic() {
     document.getElementById('modal').style.display = 'flex';
+}
+
+function onNextQuestionBtnClic() {
+    if (currentQuestionIndex >= questionsData.length - 1) {
+        return;
+    }
+    currentQuestionIndex++;
+    renderQuestion(currentQuestionIndex);
+}
+
+function onPreviousQuestionBtnClic() {
+    if (currentQuestionIndex <= 0) {
+        return;
+    }
+    currentQuestionIndex--;
+    renderQuestion(currentQuestionIndex);
 }
 
 function cerrarModal() {
